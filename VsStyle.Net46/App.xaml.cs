2bf9abfcd0fdb0ea271c6066720b2ca523960aef
JERRY TONG <jerrytong_90@hotmail.com>
2015-12-15 02:10:02 +0800

    添加窗体发光边框效果。

--- VsStyle.Net46/App.xaml.cs
+++ VsStyle.Net46/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace VsStyle.Net46
 {
@@ -13,5 +14,26 @@ namespace VsStyle.Net46
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnActivated(EventArgs e)
+        {
+            var window = this.MainWindow as MainWindow;
+            if (window != null)
+            {
+                window.Resources["BorderThemeColor"] = new SolidColorBrush(Color.FromArgb(0xFF, 0xCA, 0x51, 0x00));
+            }
+
+            base.OnActivated(e);
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            var window = this.MainWindow as MainWindow;
+            if(window != null)
+            {
+                window.Resources["BorderThemeColor"] = new SolidColorBrush(Color.FromArgb(0xFF, 0x3C, 0x40, 0x47));
+            }
+
+            base.OnDeactivated(e);
+        }
     }
 }
--- VsStyle.Net46/MainWindow.xaml.cs
+++ VsStyle.Net46/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,5 +25,159 @@ namespace VsStyle.Net46
         {
             InitializeComponent();
         }
+
+    private void WindowBorderDragDelta(object sender, DragDeltaEventArgs e)
+    {
+        var source = e.Source as System.Windows.Controls.Primitives.Thumb;
+        if (source == null) { return; }
+
+        var row = Grid.GetRow(source.Parent as UIElement);
+        var column = Grid.GetColumn(source.Parent as UIElement);
+
+        if (row == 0 && column == 0)
+        {
+            // Left Top 
+            if (this.Width - e.HorizontalChange < this.MinWidth)
+            {
+                this.Width = this.MinWidth;
+                this.Left += this.Width - this.MinWidth;
+            }
+            else
+            {
+                this.Width -= e.HorizontalChange;
+                this.Left += e.HorizontalChange;
+            }
+
+            if (this.Height - e.VerticalChange < this.MinHeight)
+            {
+                this.Height = this.MinHeight;
+                this.Top += this.Height - this.MinHeight;
+            }
+            else
+            {
+                this.Height -= e.VerticalChange;
+                this.Top += e.VerticalChange;
+            }
+        }
+        else if(row == 0 && column == 1)
+        {
+            // Top
+            if (this.Height - e.VerticalChange < this.MinHeight)
+            {
+                this.Height = this.MinHeight;
+                this.Top += this.Height - this.MinHeight;
+            }
+            else
+            {
+                this.Height -= e.VerticalChange;
+                this.Top += e.VerticalChange;
+            }
+        }
+        else if(row == 0 && column == 2)
+        {
+            // Top Right
+            if (this.Height - e.VerticalChange < this.MinHeight)
+            {
+                this.Height = this.MinHeight;
+                this.Top += this.Height - this.MinHeight;
+            }
+            else
+            {
+                this.Height -= e.VerticalChange;
+                this.Top += e.VerticalChange;
+            }
+
+            if (this.Width + e.HorizontalChange < this.MinWidth)
+            {
+                this.Width = this.MinWidth;
+            }
+            else
+            {
+                this.Width += e.HorizontalChange;
+            }
+        }
+        else if(row == 1 && column == 0)
+        {
+            // Left
+            if (this.Width - e.HorizontalChange < this.MinWidth)
+            {
+                this.Width = this.MinWidth;
+                this.Left += this.Width - this.MinWidth;
+            }
+            else
+            {
+                this.Width -= e.HorizontalChange;
+                this.Left += e.HorizontalChange;
+            }
+        }
+        else if(row == 1 && column == 2)
+        {
+            // Right
+            if (this.Width + e.HorizontalChange < this.MinWidth)
+            {
+                this.Width = this.MinWidth;
+            }
+            else
+            {
+                this.Width += e.HorizontalChange;
+            }
+        }
+        else if(row == 2 && column == 0)
+        {
+            // Left Bottom
+            if (this.Width - e.HorizontalChange < this.MinWidth)
+            {
+                this.Width = this.MinWidth;
+                this.Left += this.Width - this.MinWidth;
+            }
+            else
+            {
+                this.Width -= e.HorizontalChange;
+                this.Left += e.HorizontalChange;
+            }
+
+            if (this.Height + e.VerticalChange < this.MinHeight)
+            {
+                this.Height = this.MinHeight;
+            }
+            else
+            {
+                this.Height += e.VerticalChange;
+            }
+        }
+        else if(row == 2 && column == 1)
+        {
+            // Bottom
+            if (this.Height + e.VerticalChange < this.MinHeight)
+            {
+                this.Height = this.MinHeight;
+            }
+            else
+            {
+                this.Height += e.VerticalChange;
+            }
+        }
+        else if(row == 2 && column == 2)
+        {
+            // Right Bottom
+            if (this.Width + e.HorizontalChange < this.MinWidth)
+            {
+                this.Width = this.MinWidth;
+            }
+            else
+            {
+                this.Width += e.HorizontalChange;
+            }
+
+            if (this.Height + e.VerticalChange < this.MinHeight)
+            {
+                this.Height = this.MinHeight;
+            }
+            else
+            {
+                this.Height += e.VerticalChange;
+            }
+        }
+    }
     }
 }
